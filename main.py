6c7f658d9bab8d60bd400938c0999f82a2fb8235
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2023-11-29 19:51:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/AbhiModszYT/StringHackbot")
+os.system("git clone https://github.com/badmunda011/BADHACK")
 os.system("cd StringHackbot && python3 bot.py")
